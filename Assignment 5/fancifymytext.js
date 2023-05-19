11304b0af44718dd57b18da8f6f19639e5f0f5f4
Sean <kingkinot2@gmail.com>
2023-05-19 15:02:20 -0700

    Assignment 5

---
+++ Assignment 5/fancifymytext.js
@@ -0,0 +1,33 @@
+function alerts() {
+    alert("Hello, world!");
+}
+
+function bigger() {
+    alerts()
+    document.getElementById("inputText-1").style.fontSize = "24pt";
+}
+
+function makeMoo() {
+    const text = document.getElementById("inputText-1").value.toUpperCase();
+    const words = text.split(".");
+    for (var i = 0; i < words.length-1; i++) {
+        let word = words[i];
+        word += "-Moo";
+        words[i] = word;
+    }
+    document.getElementById("inputText-1").value = words.join("");
+}
+
+function fancy() {
+    alerts()
+    document.getElementById("inputText-1").style.fontWeight = "bold";
+    document.getElementById("inputText-1").style.color = "blue";
+    document.getElementById("inputText-1").style.textDecoration = "underline";
+}
+
+function boring() {
+    alerts()
+    document.getElementById("inputText-1").style.fontWeight = "normal";
+    document.getElementById("inputText-1").style.color = "black";
+    document.getElementById("inputText-1").style.textDecoration = "none";
+}

bed6bbd6bf83d4d32d88a869ea9998dc004fc92d
SeanTirado <Kingkinot2@gmail.com>
2025-02-11 20:23:53 -0800

    Mobile Fix -> Yes button scaling when No button is pressed

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const noButton = document.getElementById('no-button')
-const yesButton = document.getElementById('yes-button')
-const OFFSET = 30
+document.addEventListener('DOMContentLoaded', () => {
+  const noButton = document.getElementById('no-button')
+  const yesButton = document.getElementById('yes-button')
+  const OFFSET = 30
 
-let scaleFactor = 1.5
+  let scaleFactor = 1.5
 
-noButton.addEventListener('click', ()=> {
-  scaleFactor += 1.5; 
-  yesButton.style.transform = `scale(${scaleFactor})`;
-});
+  noButton.addEventListener('click', ()=> {
+    scaleFactor += 1.5; 
+    yesButton.style.transform = `scale(${scaleFactor})`;
+  });
 
 document.addEventListener('mousemove', (e) => {
     const x = e.pageX
@@ -55,3 +56,4 @@ function setButtonPosition(left, top) {
   function distanceFromCenter(boxPosition, mousePosition, boxSize) {
     return boxPosition - mousePosition + boxSize / 2
   }
+});
